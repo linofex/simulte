b524804206944f2f4305b7ad97f88ec8e3299410
linofex <alessandro.noferi@gmail.com>
2020-10-30 23:51:27 +0100

    httpResponsePacket v2

--- src/apps/mec/MeServices/packets/HttpResponsePacket.cc
+++ src/apps/mec/MeServices/packets/HttpResponsePacket.cc
@@ -1,1 +1,92 @@
-#include "HttpResponsePacket.h"
+#include "apps/mec/MeServices/packets/HttpResponsePacket.h"
+#include "inet/common/INETDefs.h"
+
+
+
+HTTPResponsePacket::HTTPResponsePacket(const char *name, short kind): HTTPResponsePacket_Base(name,kind)
+{
+     setContentType("application/json");
+     setConnection("keep-alive");
+     setBody("\r\n");
+     setPayload("");
+}
+HTTPResponsePacket::HTTPResponsePacket(const responseCode res, const char *name, short kind): HTTPResponsePacket_Base(name, kind)
+{
+    setStatus(res);
+    setContentType("application/json");
+    setConnection("keep-alive");
+    setPayload("");
+
+}
+
+void HTTPResponsePacket::setStatus(const responseCode res){
+    ::omnetpp::opp_string response;
+    switch(res) {
+        case(OK):
+            response = "200 OK";
+            break;
+        case(BAD_REQ):
+            response = "400 BadRequest";
+            break;
+        case(UNAUTH):
+            response = "401 Unauthorized";
+            break;
+        case(FORBIDDEN):
+            response = "403 Forbidden";
+            break;
+        case(NOT_FOUND):
+            response = "404 Not Found";
+            break;
+        case(NOT_ACC):
+            response = "406 Not Acceptable";
+            break;
+        case(TOO_REQS):
+            response = "429 Too Many Requests";
+            break;
+        case(BAD_METHOD):
+                response = "405 Method Not Allowed";
+                break;
+        case(HTTP_NOT_SUPPORTED):
+                response = "505 HTTP Version Not Supported";
+                break;
+        default:
+            throw cRuntimeError("Response code not allowed");
+    }
+    this->status = httpVersion + response + "\r\n";
+}
+
+void HTTPResponsePacket::setContentType(const char* contentType_){
+    contentType = ::omnetpp::opp_string("Content-Type: ") + ::omnetpp::opp_string(contentType_) + "\r\n";
+}
+
+void HTTPResponsePacket::setConnection(const char* connection_){
+    connection = ::omnetpp::opp_string("Connection: ") + ::omnetpp::opp_string(connection_) + "\r\n";
+}
+
+void HTTPResponsePacket::setBody(const char * body_){
+    body = ::omnetpp::opp_string("\r\n") + ::omnetpp::opp_string(body_) + "\r\n";
+}
+
+void HTTPResponsePacket::setBody(const std::string& body_){
+    body = ::omnetpp::opp_string("\r\n") + ::omnetpp::opp_string(body_) + "\r\n";
+}
+
+void HTTPResponsePacket::setHeaderField(const char* hfield){
+    headerFields.push_back(hfield);
+}
+
+const char* HTTPResponsePacket::getPayload(){
+    this->payload = status + contentType + connection;
+    if(!headerFields.empty()){
+        std::vector<const char *>::const_iterator it = headerFields.begin();
+        std::vector<const char *>::const_iterator end = headerFields.end();
+        for(; it != end; ++it){
+            payload += ::omnetpp::opp_string(*it) + "\r\n";
+        }
+    }
+        this->payload += body;
+
+    return this->payload.c_str();
+}
+
+
--- src/apps/mec/MeServices/packets/HttpResponsePacket.h
+++ src/apps/mec/MeServices/packets/HttpResponsePacket.h
@@ -1,19 +1,78 @@
+/**
+ * @author Alessandro Noferi
+ *
+ * This class create and manages HTTP responses, i.e.:
+ *
+ * HTTP/1.1 CODE \r\n
+ * headers[] \r\n
+ * \r\n
+ * body
+ *
+ *
+ * The required headers for our scope are:
+ * - Content-Type
+ * - Connection
+ * They are managed by specific setters and getters.
+ * It is also possible add other headers via generic method (setHeaderField)
+ *
+ */
+
+#ifndef _HTTRESPONSEPACKET_H_
+#define  _HTTRESPONSEPACKET_H_
+
+
+
 #include <omnetpp.h>
+#include <string>
 #include "apps/mec/MeServices/packets/HttpResponsePacket_m.h"
+
+enum responseCode { OK,         // 200
+                    BAD_REQ,    // 400
+                    UNAUTH,     // 401
+                    FORBIDDEN,  // 403
+                    NOT_FOUND,  // 404
+                    NOT_ACC,    // 406
+                    TOO_REQS,   // 429
+                    BAD_METHOD, // 405
+                    HTTP_NOT_SUPPORTED // 505
+                  };
+
+
+/**
+ * in HTTPResponsePacket_Base
+ * {
+ *     \@customize(true);
+ *     string status;
+ *     string connection;
+ *     string contentType;
+ *     string body;
+ *     string payload;
+ * }
+ */
 
 class HTTPResponsePacket : public HTTPResponsePacket_Base
 {
     public:
-        virtual const char * getStatus() const override;
-        virtual void setStatus(const char * status) override;
-        virtual void setStatus(const response res) override;
-        virtual const char * getConnection() const override;
-        virtual void setConnection(const char * connection) override;
-        virtual const char * getContentType() const;
-        virtual void setContentType(const char * contentType);
-        virtual const char * getBody() const;
-        virtual void setBody(const char * body);
-        virtual const char * getPayload() const;
-        virtual void setPayload(const char * payload);
+        HTTPResponsePacket(const char *name=nullptr, short kind=0);
+        HTTPResponsePacket(const responseCode res, const char *name=nullptr, short kind=0);
+        HTTPResponsePacket& operator=(const HTTPResponsePacket& other) {if (this==&other) return *this; HTTPResponsePacket_Base::operator=(other); copy(other); return *this;}
+
+        virtual HTTPResponsePacket *dup() const override {return new HTTPResponsePacket(*this);}
+
+//        virtual void setStatus(const char * status);
+        virtual void setStatus(const responseCode res);
+        virtual void setConnection(const char * connection_) override;
+        virtual void setContentType(const char * contentType_) override;
+        virtual void setBody(const char * body_) override;
+        virtual void setBody(const std::string& body_);
+        virtual void setHeaderField(const char * hfield_);
+
+        virtual const char * getPayload();
+    private:
+          //void copy(const HTTPResponsePacket& other){};
+          std::vector<const char *> headerFields;
+          ::omnetpp::opp_string httpVersion = "HTTP/1.1 ";
 
 };
+
+#endif //_HTTRESPONSEPACKET_H_
